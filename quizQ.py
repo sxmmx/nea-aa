b4f1913af7997410a11afcee707c627189f81a7f
sxmmx <120115159+sxmmx@users.noreply.github.com>
2023-12-23 23:34:15 +0000

    Create quizQ.py

---
+++ quizQ.py
@@ -0,0 +1,72 @@
+quiz_data = [
+    {
+        "question": "What separates DNA strands?",
+        "choices": ["DNA Helicase", "RNA Polymerase", "Catalyase"],
+        "answer": "RNA Polymerase"
+    },
+    {
+        "question": "What type of bonds get broken between complementary base pairs?",
+        "choices": ["Phosphodiestaer Bonds", "Ester Bonds", "Hydrogen Bonds"],
+        "answer": "Hydrogen Bonds"
+    },
+    {
+        "question": "The template is called the ____ strand.",
+        "choices": ["antisense", "sense", "Antiparallel"],
+        "answer": "antisense"
+    },
+    {
+        "question": "Free nucleotides line up by complementary base pairing and adjacent nucleotides are joined by ____ bonds",
+        "choices": ["sulphuric", "phosphodiester", "ester"],
+        "answer": "antisense"
+    },
+    {
+        "question": "What molecule is made?",
+        "choices": ["DNA", "tRNA", "mRNA"],
+        "answer": "mRNA"
+    },
+    {
+        "question": "How does it leave to attach to the ribosomes",
+        "choices": ["nuclear pore", "channel protien", "diffusion"],
+        "answer": "nuclear pore"
+    }
+]
+
+quiz_data1 = [
+    {
+        "question": "What bonds keep amino acids together?",
+        "choices": ["Hydrogen", "Peptide", "Ester"],
+        "answer": "Peptide"
+    },
+    {
+        "question": "tRNA binds to specific amino acids from the cytoplasm depending on its ____.",
+        "choices": ["template strand", "codon", "anticodon"],
+        "answer": "anticodon"
+    },
+    {
+        "question": "What is the 't'in tRNA",
+        "choices": ["transfer", "triple", "teletubbies"],
+        "answer": "transfer"
+    },
+    {
+        "question": "Free nucleotides line up by complementary base pairing and adjacent nucleotides are joined by ____ bonds",
+        "choices": ["sulphuric", "phosphodiester", "ester"],
+        "answer": "phosphodiester"
+    },
+    {
+        "question": "____ anticodons of tRNA bind to mRNA codons and are held in place by hydrogen bonds.",
+        "choices": ["Opposite", "Complementary", "Random"],
+        "answer": "Complementary"
+    },
+    {
+        "question": "The ribosome joins the amino acids aached to two tRNA molecules by a pepde bond and then tRNA molecules detach from the amino acids. This process is repeated until ____",
+        "choices": ["chain gets too long", "amino acids run out", "stop codon is reached"],
+        "answer": "stop codon is reached"
+      
+    },
+    {
+      "question": "If the DNA code reads CCAATTGG then the tRNA code would read ____",
+      "choices": ["GGTTAACC", "GGUUAACC", "CCTTAAGG"],
+      "answer": "GGTTAACC"
+
+    }
+]
