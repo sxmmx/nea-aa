8fd316e64881cc89fc1d5270d8aa0e2ef46ad10b
sxmmx <120115159+sxmmx@users.noreply.github.com>
2024-03-22 10:55:38 +0000

    Update main.sql

--- main.sql
+++ main.sql
@@ -5,3 +5,17 @@ CREATE TABLE Users(
 );
 
 INSERT INTO Users (User_ID, User_name, User_password) VALUES (?, ?, ?);
+
+CREATE TABLE Results(
+  Result_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  User_name varchar(50) NOT NULL,
+  Score INTEGER NOT NULL
+);
+
+CREATE TABLE Questions(
+  Question_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  Choice1 varchar(50) NOT NULL,
+  Choice2 varchar(50) NOT NULL,
+  Choice3 varchar(50) NOT NULL,
+  Answer varchar(50) NOT NULL
+);
